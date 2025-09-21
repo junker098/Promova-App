eeaab1a80729181e252cbd77465d1bf02978c30f
Yuriy <junker098@gmail.com>
2025-09-21 12:00:44 +0300

    Initial Commit

---
+++ Promova App/Promova_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Promova_AppApp.swift
+//  Promova App
+//
+//  Created by Yuriy on 21.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct Promova_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
